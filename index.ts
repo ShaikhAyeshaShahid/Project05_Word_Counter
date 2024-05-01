4cf62875d48529afcb66e58ecb4edb6d1a599e49
ShaikhAyeshaShahid <85065617+ShaikhAyeshaShahid@users.noreply.github.com>
2024-05-01 14:29:02 +0500

    Word counter

---
+++ index.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bold.blue("\n \t\tProject-06 Word Counter"));
+console.log("=".repeat(60));
+
+let answers = await inquirer.prompt(
+    [
+        {
+            name:"sentence",
+            type:"input",
+            message:"Enter a scentence: ",
+        }
+    ]
+);
+
+let words = answers.sentence.trim().split(" ");
+console.log("=".repeat(60));
+console.log(chalk.bold("Scentence words: "));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.red(words.length)}`));
+console.log("=".repeat(60));
